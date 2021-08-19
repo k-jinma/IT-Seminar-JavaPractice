1ff4008fc5880e7a4f750974de8b316cd4c3745d
k-jinma <jinmakazuhiro@outlook.jp>
2021-08-20 00:32:21 +0900

    暫定版

--- CashRegister/src/CashRegister.java
+++ CashRegister/src/CashRegister.java
@@ -6,110 +6,113 @@ public class CashRegister {
 
 		Scanner sc = new Scanner(System.in);
 
-		int item1Price, item2Price, item3Price; // 商品金額１～３
-		int item1Count, item2Count, item3Count; // 商品数量１～３
+		int item1Price; // 商品金額
+		int item1Count; // 商品数量
 		int syokei; // 小計
 		int tax; // 消費税
 		int azukari; // 預かり金額
+		int inputNo; // 入力番号
 
-		// メニュー画面表示
-		System.out.println("キャッシュ・レジスター　メニュー");
-		System.out.println("１：お会計");
-		System.out.println("２：終了");
-
-		// メニュー番号入力
-		int inputNo = sc.nextInt();
-		switch (inputNo) {
-
-		// お会計処理
-		case 1:
-
-			System.out.print("商品１の金額を入力してください：\\");
-			item1Price = sc.nextInt();
-			if (item1Price < 0) {
-				System.err.println("入力金額が不正です。システムを終了します。");
-				System.exit(-1);
-			}
-
-			System.out.print("商品１の数量を入力してください：");
-			item1Count = sc.nextInt();
-			if (item1Count < 0) {
-				System.err.println("入力数量が不正です。システムを終了します。");
-				System.exit(-2);
-			}
-
-			System.out.print("商品２の金額を入力してください：\\");
-			item2Price = sc.nextInt();
-			if (item2Price < 0) {
-				System.err.println("入力金額が不正です。システムを終了します。");
-				System.exit(-1);
-			}
-
-			System.out.print("商品２の数量を入力してください：");
-			item2Count = sc.nextInt();
-			if (item2Count < 0) {
-				System.err.println("入力数量が不正です。システムを終了します。");
-				System.exit(-2);
-			}
-
-			System.out.print("商品３の金額を入力してください：\\");
-			item3Price = sc.nextInt();
-			if (item3Price < 0) {
-				System.err.println("入力金額が不正です。システムを終了します。");
-				System.exit(-1);
-			}
-
-			System.out.print("商品３の数量を入力してください：");
-			item3Count = sc.nextInt();
-			if (item3Count < 0) {
-				System.err.println("入力数量が不正です。システムを終了します。");
-				System.exit(-2);
-			}
-
-			// 小計の計算と表示
-			syokei = item1Price * item1Count + item2Price * item2Count + item3Price * item3Count;
-			System.out.println("小計：\\" + syokei);
-
-			// 消費税の計算と表示
-			tax = (int) (syokei * 0.1);
-			System.out.println("消費税：\\" + tax);
-
-			// 合計額の表示
-			System.out.println("合計：\\" + (syokei + tax));
-
-			// お預かり金額の入力
-			System.out.print("お預かり：");
-			azukari = sc.nextInt();
-			if (azukari < 0) {
-				System.err.println("入力金額が不正です。システムを終了します。");
-				System.exit(-1);
-
-			} else if (azukari < (syokei + tax)) {
-				System.err.println("お預かり金額が合計額より少ないです。システムを終了します");
-				System.exit(-3);
-			}
-
-			System.out.println("お釣り：\\" + (azukari - syokei - tax));
-
-			break;
-
+		
+		while( true ){
 			
-		// システム終了
-		case 2:
+			// メニュー画面表示
+			System.out.println("------------------------------");
+			System.out.println("キャッシュ・レジスター　メニュー");
+			System.out.println("１：お会計");
+			System.out.println("２：終了");
+			System.out.println("------------------------------");
 			
-			System.out.println("システムを終了します。");
-			break;
+			// メニュー番号入力
+			inputNo = sc.nextInt();
+		
+			switch (inputNo) {
 
-		//システム終了
-		default:
-			System.out.println("1または2以外の番号が入力されました。システムを終了します。");
-			break;
+			// お会計処理
+			case 1:
+				
+				// 商品点数
+				int cnt = 1;
+				// 小計をリセット
+				syokei = 0;
+				
+				do{
 
+					while(true){
+						System.out.print("商品" + cnt + "の金額を入力してください：\\");
+						item1Price = sc.nextInt();
+						if (item1Price < 0) {
+							System.err.println("入力金額が不正です。");
+							
+						}else{
+							break;
+						}
+					}
+	
+					while(true){
+						System.out.print("商品" + cnt + "の数量を入力してください：");
+						item1Count = sc.nextInt();
+						if (item1Count < 0) {
+							System.err.println("入力数量が不正です。");
+
+						}else{
+							break;
+						}
+
+					}
+					// 小計の計算
+					syokei += (item1Price * item1Count);
+	
+					System.out.println("次の商品？(Yes=1 / No=2)：");
+					inputNo = sc.nextInt();
+
+					// 次の入力用に商品点数をプラス
+					cnt++;
+
+				}while(inputNo == 1);
+				
+				// 小計の表示
+				System.out.println("小計：\\" + syokei);
+
+				// 消費税の計算と表示
+				tax = (int) (syokei * 0.1);
+				System.out.println("消費税：\\" + tax);
+
+				// 合計額の表示
+				System.out.println("合計：\\" + (syokei + tax));
+
+				// お預かり金額の入力
+				System.out.print("お預かり：");
+				azukari = sc.nextInt();
+				if (azukari < 0) {
+					System.err.println("入力金額が不正です。システムを終了します。");
+					System.exit(-1);
+
+				} else if (azukari < (syokei + tax)) {
+					System.err.println("お預かり金額が合計額より少ないです。システムを終了します");
+					System.exit(-3);
+				}
+
+				System.out.println("お釣り：\\" + (azukari - syokei - tax));
+
+				break;
+
+				
+			// システム終了
+			case 2:
+				
+				System.out.println("システムを終了します。");
+				//入力ストリームのクローズ
+				sc.close();
+				System.exit(0);
+
+			//システム終了
+			default:
+				System.out.println("正しい番号を入力してください。");
+				
+			}
 		}
-		
-		
-		sc.close();
-
+	
 	}
 
 }
